d6ad807077e50dcdb199d68670c4c27a9fcc071d
marcolarotonda <marco.larotonda.1@gmail.com>
2024-04-14 22:45:56 +0200

    first commit

---
+++ src/main/java/io/github/marcolarotonda/discordbotdnd5e/DiscordbotDnd5eApplication.java
@@ -0,0 +1,13 @@
+package io.github.marcolarotonda.discordbotdnd5e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordbotDnd5eApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordbotDnd5eApplication.class, args);
+	}
+
+}
