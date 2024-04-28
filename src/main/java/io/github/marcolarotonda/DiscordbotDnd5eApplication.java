5f81a5a6c5a3b038665c9735576a57e0b213ab6b
marcolarotonda <marco.larotonda.1@gmail.com>
2024-04-28 19:22:12 +0200

    fix main

--- src/main/java/io/github/marcolarotonda/DiscordbotDnd5eApplication.java
+++ src/main/java/io/github/marcolarotonda/DiscordbotDnd5eApplication.java
@@ -1,13 +1,16 @@
 package io.github.marcolarotonda;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiscordbotDnd5eApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscordbotDnd5eApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(DiscordbotDnd5eApplication.class);
+		springApplication.setWebApplicationType(WebApplicationType.NONE);
+		springApplication.run(args);
 	}
 
 }
