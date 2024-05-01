90968236a07333fee32ae009ed2ad796f305f9ab
marcolarotonda <marco.larotonda.1@gmail.com>
2024-05-01 12:00:45 +0200

    aggiunto avatar

--- src/main/java/io/github/marcolarotonda/discordbotdnd5e/BotRunner.java
+++ src/main/java/io/github/marcolarotonda/discordbotdnd5e/BotRunner.java
@@ -1,13 +1,17 @@
 package io.github.marcolarotonda.discordbotdnd5e;
 
 import io.github.marcolarotonda.discordbotdnd5e.service.Listener;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Icon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 
 @Service
@@ -25,9 +29,14 @@ public class BotRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        JDABuilder.createLight(token)
+        JDA jda = JDABuilder.createLight(token)
                 .setStatus(OnlineStatus.ONLINE)
                 .addEventListeners(listener)
                 .build();
+        jda.getSelfUser()
+                .getManager()
+                .reset()
+                .setAvatar(Icon.from(new File("src/main/resources/bot-avatar.webp")))
+                .queue();
     }
 }
